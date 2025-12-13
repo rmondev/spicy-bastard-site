bd61d1cbdd3924f3d0cac4abf6667afbaa7e0508
Riccardo Moncada <rmondev@gmail.com>
2025-12-13 00:06:40 -0500

    change variable name

--- components/FeedbackCarousel.tsx
+++ components/FeedbackCarousel.tsx
@@ -134,7 +134,7 @@ export default function FeedbackCarousel() {
       process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
       process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
       { message },
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+      process.env.NEXT_PUBLIC_EMAILJS_P_KEY!
     );
 
     console.log("EmailJS response:", message);
